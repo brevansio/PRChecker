e6f7c93fc497d7ec5e85328ce45d70e18fcb9d3a
Bruce Evans <bruce.evans.dev@gmail.com>
2021-11-06 10:59:21 +0900

    Present the Login view on the main screen
    Signed-off-by: Bruce Evans <bruce.evans.dev@gmail.com>

--- PRChecker/Shared/ContentView.swift
+++ PRChecker/Shared/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showLogin = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            HStack {
+                VStack {
+                    Spacer()
+                    Button {
+                        showLogin.toggle()
+                    } label: {
+                        Text(.init(systemName: "person"))
+                            .font(.system(size: 45))
+                            .foregroundColor(.secondary)
+                    }
+                    .buttonStyle(.link)
+                    .popover(isPresented: $showLogin, arrowEdge: .trailing) {
+                        LoginView {
+                            // TODO: Refresh
+                        }
+                    }
+
+                    Button {
+                        // TODO: Settings Page
+                    } label: {
+                        Text(.init(systemName: "gearshape"))
+                            .font(.system(size: 45))
+                            .foregroundColor(.secondary)
+                    }
+                        .buttonStyle(.link)
+                }
+                .frame(alignment: .leading)
+                .border(BackgroundStyle(), width: 5.0)
+                Spacer()
+                Text("Hello, world!")
+                Spacer()
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
     }
 }
 
