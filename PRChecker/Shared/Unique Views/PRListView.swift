92bf4ac7f909038a2c1881d600265c1aafaa1a04
Bruce Evans <bruce.evans.dev@gmail.com>
2021-11-07 22:54:30 +0900

    Add sections and display multiple prlists at once

--- PRChecker/Shared/Unique Views/PRListView.swift
+++ PRChecker/Shared/Unique Views/PRListView.swift
@@ -12,26 +12,48 @@ struct PRListView: View {
     @EnvironmentObject var filterViewModel: FilterViewModel
     
     @ObservedObject var prListViewModel = PRListViewModel()
+    @StateObject var watchedUserViewModel = WatchedUserViewModel()
     
     var body: some View {
         RefreshableScrollView(onRefresh: { completion in
             prListViewModel.getPRList() {
                 completion()
             }
+            watchedUserViewModel.getPRList()
         }) {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 300))], alignment: .leading) {
-                ForEach(prListViewModel.prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), id: \.id) { pullRequest in
-                    PullRequestCell(pullRequest: pullRequest)
-                        .cornerRadius(16)
-                        .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray3, lineWidth: 1))
-                        .onTapGesture {
-                            openURL(URL(string: pullRequest.url)!)
-                        }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 300))], alignment: .leading, pinnedViews: [.sectionHeaders]) {
+                
+                Section(header: Rectangle().frame(height: 45).foregroundColor(.gray5).overlay(Text("You").font(.title).bold().padding(.leading), alignment: .leading)) {
+                    ForEach(prListViewModel.prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), id: \.id) { pullRequest in
+                        PullRequestCell(pullRequest: pullRequest)
+                            .cornerRadius(16)
+                            .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray3, lineWidth: 1))
+                            .onTapGesture {
+                                openURL(URL(string: pullRequest.url)!)
+                            }
+                    }
                 }
+                
+                ForEach(watchedUserViewModel.prList, id: \.0) { (name, prList) in
+                    Section(header: Rectangle().frame(height: 45).foregroundColor(.gray5).overlay(Text(name).font(.title).bold().padding(.leading), alignment: .leading)) {
+                        ForEach(prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), id: \.id) { pullRequest in
+                            PullRequestCell(pullRequest: pullRequest)
+                                .cornerRadius(16)
+                                .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray3, lineWidth: 1))
+                                .onTapGesture {
+                                    openURL(URL(string: pullRequest.url)!)
+                                }
+                        }
+                    }
+                }
+                
+                
+    
             }
             .padding()
             .onAppear {
                 prListViewModel.getPRList()
+                watchedUserViewModel.getPRList()
             }
             .onChange(of: prListViewModel.additionalFilters) { newValue in
                 if let labelSection = filterViewModel.sections.first(where: { $0.name == "Labels" }) {
