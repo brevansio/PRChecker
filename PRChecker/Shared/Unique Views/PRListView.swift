dfc48c9a2ce8903c6c23b58f76496e9f8d597072
Bruce Evans <bruce.evans.dev@gmail.com>
2021-11-08 19:13:53 +0900

    Add Label and Repo filters from watched members

--- PRChecker/Shared/Unique Views/PRListView.swift
+++ PRChecker/Shared/Unique Views/PRListView.swift
@@ -23,20 +23,9 @@ struct PRListView: View {
         }) {
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 300))], alignment: .leading, pinnedViews: [.sectionHeaders]) {
                 
-                Section(header: Rectangle().frame(height: 45).foregroundColor(.gray5).overlay(Text("You").font(.title).bold().padding(.leading), alignment: .leading)) {
-                    ForEach(prListViewModel.prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), id: \.id) { pullRequest in
-                        PullRequestCell(pullRequest: pullRequest)
-                            .cornerRadius(16)
-                            .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray3, lineWidth: 1))
-                            .onTapGesture {
-                                openURL(URL(string: pullRequest.url)!)
-                            }
-                    }
-                }
-                
-                ForEach(watchedUserViewModel.prList, id: \.0) { (name, prList) in
-                    Section(header: Rectangle().frame(height: 45).foregroundColor(.gray5).overlay(Text(name).font(.title).bold().padding(.leading), alignment: .leading)) {
-                        ForEach(prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), id: \.id) { pullRequest in
+                if let filteredPRList = prListViewModel.prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), !filteredPRList.isEmpty {
+                    Section(header: Rectangle().frame(height: 45).foregroundColor(.gray5).overlay(Text("You").font(.title).bold().padding(.leading), alignment: .leading)) {
+                        ForEach(filteredPRList, id: \.id) { pullRequest in
                             PullRequestCell(pullRequest: pullRequest)
                                 .cornerRadius(16)
                                 .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray3, lineWidth: 1))
@@ -48,22 +37,42 @@ struct PRListView: View {
                 }
                 
                 
-    
+                ForEach(watchedUserViewModel.prList, id: \.0) { (name, prList) in
+                    if let filteredPRList = prList.filter(filterViewModel.combinedFilter?.filter ?? { _ in true }), !filteredPRList.isEmpty {
+                        
+                        Section(header: Rectangle().frame(height: 45).foregroundColor(.gray5).overlay(Text(name).font(.title).bold().padding(.leading), alignment: .leading)) {
+                            ForEach(filteredPRList, id: \.id) { pullRequest in
+                                PullRequestCell(pullRequest: pullRequest)
+                                    .cornerRadius(16)
+                                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray3, lineWidth: 1))
+                                    .onTapGesture {
+                                        openURL(URL(string: pullRequest.url)!)
+                                    }
+                            }
+                        }
+                    }
+                    
+                }
             }
             .padding()
             .onAppear {
                 prListViewModel.getPRList()
                 watchedUserViewModel.getPRList()
             }
-            .onChange(of: prListViewModel.additionalFilters) { newValue in
-                if let labelSection = filterViewModel.sections.first(where: { $0.name == "Labels" }) {
-                    labelSection.filters = newValue?["Labels"] ?? []
-                }
-                
-                if let repositorySection = filterViewModel.sections.first(where: { $0.name == "Repository" }) {
-                    repositorySection.filters = newValue?["Repository"] ?? []
-                }
-            }
+            .onChange(of: prListViewModel.additionalFilters, perform: updateAdditionalFilters(_:))
+            .onChange(of: watchedUserViewModel.additionalFilters, perform: updateAdditionalFilters(_:))
+        }
+    }
+    
+    private func updateAdditionalFilters(_ filters: [String: [Filter]]?) {
+        if let labelSection = filterViewModel.sections.first(where: { $0.name == "Labels" }) {
+            let combinedFilters = labelSection.filters + (filters?["Labels"] ?? [])
+            labelSection.filters = combinedFilters.arrayByRemovingDuplicates().sorted { $0.name < $1.name }
+        }
+        
+        if let repositorySection = filterViewModel.sections.first(where: { $0.name == "Repository" }) {
+            let combinedFilters = repositorySection.filters + (filters?["Repository"] ?? [])
+            repositorySection.filters = combinedFilters.arrayByRemovingDuplicates().sorted { $0.name < $1.name }
         }
     }
 }
--- PRChecker/Shared/ViewModels/WatchedUserViewModel.swift
+++ PRChecker/Shared/ViewModels/WatchedUserViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class WatchedUserViewModel: ObservableObject {
     @Published var prList = [(String, [PullRequest])]()
+    @Published var additionalFilters: [String: [Filter]]?
     
     private var subscriptions = Set<AnyCancellable>()
     private var userList: [String]
@@ -24,7 +25,26 @@ class WatchedUserViewModel: ObservableObject {
                 // TODO: Handle Error
             } receiveValue: { newEntry in
                 DispatchQueue.main.async {
-                    self.prList.append(newEntry)
+                    self.prList = (self.prList + [newEntry]).sorted { $0.0 < $1.0 }
+                    
+                    let labelFilters = self.prList.map(\.1).flatMap { $0 }.map(\.labels).flatMap { $0 }.map { label in
+                        Filter(name: label.title) { pullRequest in
+                            pullRequest.labels.contains { prLabel in
+                                prLabel == label
+                            }
+                        }
+                    }
+                        .arrayByRemovingDuplicates()
+                    
+                    let repositoryFilters = self.prList.map(\.1).flatMap { $0 }.map(\.repositoryName).map { name in
+                        Filter(name: name) { $0.repositoryName == name }
+                    }
+                        .arrayByRemovingDuplicates()
+                    
+                    self.additionalFilters = [
+                        "Labels": labelFilters,
+                        "Repository": repositoryFilters,
+                    ]
                 }
             }
             .store(in: &subscriptions)
