17caf083a3b366f5a8691777b248377ca60a7673
Ckitakishi <chenyuhan43@gmail.com>
2021-11-25 19:26:44 +0900

    Move the setting icon to tool bar

--- PRChecker/Shared/Unique Views/ContentView.swift
+++ PRChecker/Shared/Unique Views/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var showLogin = !SettingsViewModel.shared.loginViewModel.canLogin
     @State var showSettings = false
+    
     var prListViewModel = PRListViewModel()
     
     var body: some View {
@@ -27,45 +28,37 @@ struct ContentView: View {
                         alignment: .topLeading
                     )
             }
-            .overlay(
-                HStack {
-                    VStack {
-                        Spacer()
-                        
-                        Button {
-                            showSettings.toggle()
-                        } label: {
-                            Text(.init(systemName: "gearshape"))
-                                .font(.system(size: 45))
-                                .fontWeight(.thin)
-                                .foregroundColor(.secondary)
-                        }
-                        .buttonStyle(LinkButtonStyle())
-                        .popover(isPresented: $showSettings, arrowEdge: .bottom) {
-                            SettingsView()
-                                .frame(minWidth: 200, maxHeight: 800, alignment: .leading)
-                                .onDisappear { prListViewModel.getPRList() }
-                        }
-                        .padding(8)
-                        .background(Color.gray4.clipShape(Circle()))
-                    }
-                    .padding([.leading, .bottom])
-                    
-                    Spacer()
-                }
-            )
         }.toolbar {
             ToolbarItem(placement: .navigation) {
                 Button(action: toggleSidebar, label: {
                     Image(systemName: "sidebar.leading")
+                        .renderingMode(.template)
+                        .foregroundColor(Color.primary)
                 })
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button{
+                    showSettings.toggle()
+                } label: {
+                    Image(systemName: "gearshape")
+                        .renderingMode(.template)
+                        .foregroundColor(Color.primary)
+                }
+                .popover(isPresented: $showSettings, arrowEdge: .bottom) {
+                    SettingsView()
+                        .frame(minWidth: 200, maxHeight: 800, alignment: .leading)
+                        .onDisappear { prListViewModel.getPRList() }
+                }
             }
         }
     }
     
     private func toggleSidebar() {
         #if canImport(AppKit)
-        NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+        NSApp.keyWindow?.firstResponder?.tryToPerform(
+            #selector(NSSplitViewController.toggleSidebar(_:)),
+            with: nil
+        )
         #endif
     }
 }
